06fef13e699f7b8bdbfa880348bb43c5d7d0aaee
Lighthigh57 <78249745+Lighthigh57@users.noreply.github.com>
2022-06-12 18:35:44 +0900

    AddWebSocketAndOther

---
+++ Assets/GetConfig.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ホスト名を取得する
+        var hostname = Dns.GetHostName();
+
+        // ホスト名からIPアドレスを取得する
+        IPAddress[] adrList = Dns.GetHostAddresses(hostname);
+        foreach (IPAddress address in adrList)
+        {
+            Debug.Log(address.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WebSocketController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Net;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+public class WebSocketController : MonoBehaviour
+{
+    private WebSocketServer server;
+    // Start is called before the first frame update
+    public void LaunchServer (int port)
+    {
+        if (server == null)
+        {
+            server = new WebSocketServer(port);
+
+            server.AddWebSocketService<Echo>("/");
+            server.Start();
+        }
+        else
+        {
+            server.Stop();
+            server = null;
+        }
+        
+
+    }
+    void OnDestroy()
+    {
+        server.Stop();
+        server = null;
+    }
+}
+
+public class Echo : WebSocketBehavior
+{
+    protected override void OnMessage (MessageEventArgs e)
+    {
+        Debug.Log(e.Data);
+        Sessions.Broadcast(e.Data);
+    }
+}
